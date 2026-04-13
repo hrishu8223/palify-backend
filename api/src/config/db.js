ee892f6f079edc57cae55de3ae649bdda6684db2
Rishika Rathore <hrishu8223@gmail.com>
2026-04-13 17:42:47 +0530

    fix: hardcode mysql connection

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -1,13 +1,48 @@
+// // const mysql = require("mysql2");
+
+// // const pool = mysql.createPool({
+// //   host: "localhost",
+// //   user: "root",
+// //   password: "",
+// //   database: "palify_db",
+// // });
+
+// // // promise wrapper
+// // const db = pool.promise();
+
+// // module.exports = db;
+// const mysql = require("mysql2");
+
+// const pool = mysql.createPool({
+//   host: process.env.MYSQLHOST,
+//   user: process.env.MYSQLUSER,
+//   password: process.env.MYSQL_ROOT_PASSWORD,
+//   database: process.env.MYSQL_DATABASE,
+//   port: process.env.MYSQLPORT || 3306,
+// });
+
+// const db = pool.promise();
+
+// module.exports = db;
+// const pool = mysql.createPool({
+//   host: process.env.MYSQLHOST || 'mysql.railway.internal',
+//   user: process.env.MYSQLUSER || 'root',
+//   password: process.env.MYSQL_ROOT_PASSWORD || '',
+//   database: process.env.MYSQL_DATABASE || 'railway',
+//   port: process.env.MYSQLPORT || 3306,
+// });
+// const db = pool.promise();
+// module.exports = db;
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "palify_db",
+  host: 'mysql.railway.internal',
+  user: 'root',
+  password: 'fXEmbUfbPBfqTCoXPOidPaejlljdKrpv',
+  database: 'railway',
+  port: 3306,
 });
 
-// promise wrapper
 const db = pool.promise();
 
 module.exports = db;
